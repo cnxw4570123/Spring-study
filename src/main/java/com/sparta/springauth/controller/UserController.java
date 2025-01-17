287d47a687451087133332e521f9c58e07955021
cnxw4570123 <cnxw4570123@gmail.com>
2025-01-17 14:24:56 +0900

    Validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,24 +1,29 @@
 package com.sparta.springauth.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
 
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
-	private final UserService userSerivce;
 
-	public UserController(UserService userSerivce) {
-		this.userSerivce = userSerivce;
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 
 	@GetMapping("/user/login-page")
@@ -32,8 +37,18 @@ public class UserController {
 	}
 
 	@PostMapping("/user/signup")
-	public String singup(@ModelAttribute SignupRequestDto requestDto) {
-		userSerivce.signup(requestDto);
+	public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+		// Validation 예외처리
+		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+		if(!fieldErrors.isEmpty()) {
+			for (FieldError fieldError : bindingResult.getFieldErrors()) {
+				log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+			}
+			return "redirect:/api/user/signup";
+		}
+
+		userService.signup(requestDto);
+
 		return "redirect:/api/user/login-page";
 	}
 }
