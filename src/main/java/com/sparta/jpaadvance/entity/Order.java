e57f9486d45a71b61b268e6c9f43dfbda7b4eae5
cnxw4570123 <cnxw4570123@gmail.com>
2025-01-18 20:19:07 +0900

    중간테이블 Order 주문일 추가

--- src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -1,21 +1,18 @@
 package com.sparta.jpaadvance.entity;
 
-import java.util.List;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,9 +20,13 @@ public class Order {
 
 	@ManyToOne
 	@JoinColumn(name = "food_id")
-	private User user;
+	private Food food;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
-	private Food food;
+	private User user;
+
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime orderDate;
 }
