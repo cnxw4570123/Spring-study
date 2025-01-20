13817f07b20a9a53c3965901e0d07e9be4d08598
cnxw4570123 <cnxw4570123@gmail.com>
2025-01-20 16:30:11 +0900

    feat : scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.sparta.myselectshop.naver.config.NaverApiProperty;
 
 @SpringBootApplication
 @EnableConfigurationProperties(NaverApiProperty.class)
 @EnableJpaAuditing
+@EnableScheduling
 public class MyselectshopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.sparta.myselectshop.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +10,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -38,5 +40,13 @@ public class ProductService {
 		return products.stream()
 			.map(ProductResponseDto::new)
 			.toList();
+	}
+
+	@Transactional
+	public void updateBySearch(Long id, ItemDto itemDto) {
+		Product product = productRepository.findById(id)
+			.orElseThrow(() -> new NoSuchElementException("해당 상품은 존재하지 않습니다."));
+
+		product.updateByItemDto(itemDto);
 	}
 }
