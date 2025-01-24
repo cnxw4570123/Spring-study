27e3f1744e179d10528da14d7cd549343320b219
cnxw4570123 <cnxw4570123@gmail.com>
2025-01-24 14:22:56 +0900

    test : 컨트롤러 테스트

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,14 +3,12 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.sparta.myselectshop.naver.config.NaverApiProperty;
 
 @SpringBootApplication
 @EnableConfigurationProperties(NaverApiProperty.class)
-@EnableJpaAuditing
 @EnableScheduling
 public class MyselectshopApplication {
 
