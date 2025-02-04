ecd4044748f0fc660a63303f88ad64b9711323ef
cnxw4570123 <cnxw4570123@gmail.com>
2025-02-04 17:58:46 +0900

    init : raw-jpa 프로젝트

---
+++ rawJpa/src/main/java/com/sparta/rawjpa/RawJpaApplication.java
@@ -0,0 +1,12 @@
+package com.sparta.rawjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawJpaApplication.class, args);
+	}
+}
---
+++ rawJpa/src/test/java/com/sparta/rawjpa/RawJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.rawjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
