0851e375ad3904b5fd64de52ea351043a326e0ee
cnxw4570123 <cnxw4570123@gmail.com>
2025-02-06 12:00:35 +0900

    feat : 서킷브레이커 추가
    - Resilience4j를 활용해 서비스 호출 안정성 보장
      - 현재는 각각의 이벤트에 대해서 로그를 찍도록 하였지만, 추후 sms, slack 등등을 통해 장애상태 관리자에게 전달 가능

--- sample/src/main/java/com/sparta/springcloud/resilience4j/sample/ProductService.java
+++ sample/src/main/java/com/sparta/springcloud/resilience4j/sample/ProductService.java
@@ -4,13 +4,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-	private final Logger logger = LoggerFactory.getLogger(ProductService.class);
+	private final Logger log = LoggerFactory.getLogger(ProductService.class);
+	private final CircuitBreakerRegistry circuitBreakerRegistry;
 
+	@PostConstruct
+	public void registerEventListener() {
+		circuitBreakerRegistry.circuitBreaker("productService")
+			.getEventPublisher()
+			.onStateTransition(event -> log.info("######CircuitBreaker State Transition = {}", event))
+			// 호출 차단 이벤트 리스너
+			.onFailureRateExceeded(event -> log.info("#####CircuitBreaker Failure Rated Exceed = {}", event))
+			.onCallNotPermitted(event -> log.info("#####CircuitBreaker Call Not Permitted = {}", event))
+			.onError(event -> log.info("#####CircuitBreaker Error = {}", event));
+	}
+
+	@CircuitBreaker(name = "productService", fallbackMethod = "fallbackGetProductDetails")
 	public Product getProductDetail(String productId) {
 		if ("111".equals(productId)) {
 			throw new RuntimeException("empty response body");
@@ -18,6 +34,14 @@ public class ProductService {
 		return new Product(
 			productId,
 			"sample Product : " + productId
+		);
+	}
+
+	public Product fallbackGetProductDetails(String productId, Throwable t) {
+		log.error("#####Fallback triggered for productId : {} due to {}", productId, t.getMessage());
+		return new Product(
+			productId,
+			"Fallback Product"
 		);
 	}
 }
