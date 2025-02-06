919126c00870cced41999529f52206e0a92de73f
cnxw4570123 <cnxw4570123@gmail.com>
2025-02-06 16:25:48 +0900

    feat : Gateway 적용하기

---
+++ gateway/src/main/java/com/sparta/springcloud/eureka/client/gateway/CustomPreFilter.java
@@ -0,0 +1,29 @@
+package com.sparta.springcloud.eureka.client.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomPreFilter implements GlobalFilter, Ordered {
+	private static final Logger log = LoggerFactory.getLogger(CustomPreFilter.class);
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		log.info("PRE-filter] request URI = {}", request.getURI());
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE;
+	}
+}
--- order/src/main/java/com/sparta/springcloud/eureka/client/order/OrderController.java
+++ order/src/main/java/com/sparta/springcloud/eureka/client/order/OrderController.java
@@ -1,7 +1,6 @@
 package com.sparta.springcloud.eureka.client.order;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -11,8 +10,8 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
 	private final OrderService orderService;
 
-	@GetMapping("/order/{orderId}")
-	public String getProductInfo(@PathVariable("orderId") String orderId) {
-		return orderService.getOrder(orderId);
+	@GetMapping("/order")
+	public String getProductInfo() {
+		return "Order details";
 	}
 }
