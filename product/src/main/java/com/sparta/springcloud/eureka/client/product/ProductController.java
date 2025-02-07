7a4668e8c092a824670367d8ab2e86c9ffc72f3d
cnxw4570123 <cnxw4570123@gmail.com>
2025-02-07 16:43:47 +0900

    feat : actuator를 활용한 동적 설정 정보 변경
    - config 서버의 설정 정보 변경 후 반영하고자 하는 서버에서 actuator/refresh로 POST 요청을 넣어 반영.

--- product/src/main/java/com/sparta/springcloud/eureka/client/product/ProductController.java
+++ product/src/main/java/com/sparta/springcloud/eureka/client/product/ProductController.java
@@ -1,10 +1,12 @@
 package com.sparta.springcloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ProductController {
 
 	@Value("${server.port}")
